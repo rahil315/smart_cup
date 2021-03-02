c89b39d899c3336e43b5c02fadef495ed96654d6
Rahil Rathod <66318929+rahil315@users.noreply.github.com>
2021-03-01 19:06:08 -0500

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -35,6 +35,7 @@ import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 //import Sonnet from 'react-bootstrap/Sonnet'
 import filterFactory, { customFilter, textFilter } from 'react-bootstrap-table2-filter';  
+import { colors } from "@material-ui/core";
 
 
 
@@ -45,6 +46,9 @@ const data = [
   { day: 'thu', value: 100 },
   { day: 'fri', value: 350 },
   
+];
+const patientDummyData = [
+  { name: 'John Doe', weight: '140', height: '6', value: '500', description: 'Student'},
 ];
 
 const dailyData = [
@@ -78,9 +82,24 @@ class HomePage extends Component {
      inputName: "",
      inputWeight:"",
      inputHeight:"",
+     inputLimit:"",
+     inputDescription:"",
      storage: [],
      historyStorage: [],
-    
+     dispHistoryName:"",
+     histName:"",
+     rows: [],
+     name:"",
+     Name:'',
+     weight:'',
+     height:'',
+     value:'',
+     description:'',
+     patientDummyData: [],
+     historyTable:[],
+     nameVal:'',
+     sendName:'',
+
      columns : [{
       dataField: 'name',
       text: 'name'
@@ -94,10 +113,7 @@ class HomePage extends Component {
       sort: true
     }],
 
-    columns1 : [{
-      dataField: 'name',
-      text: 'Name'
-    }, {
+    columns1 : [ {
       dataField: 'Date',
       text: 'Date',
       sort: true
@@ -138,29 +154,33 @@ class HomePage extends Component {
   handleSubmit(e) {
     e.preventDefault();
    
+   /*
     console.log("The form was submitted with the following data:");
     console.log(this.state.inputName);
     console.log(this.state.inputWeight);
     console.log(this.state.inputHeight);
-    
-    const { inputName, inputWeight, inputHeight } = this.state;
+    console.log(this.state.inputLimit);
+    console.log(this.state.inputDescription);
+    const { inputName, inputWeight, inputHeight, inputLimit, inputDescription } = this.state;
 
     const detailPatient = {
       inputName,
       inputWeight,
       inputHeight,
+      inputLimit,
+      inputDescription
     };
     axios
       .post('http://localhost:9000/users/patientDetails', detailPatient)
       .then(() => {
-      console.log('data Created')
+      console.log('data Created: ',detailPatient)
     })
       .catch(err => {
         console.log('Not working!')
         console.error(err);
       });
       this.hideModal();
-      
+     */ 
   }
 
 
@@ -178,15 +198,11 @@ class HomePage extends Component {
   }
 
   componentDidMount () {
+    
     axios
     .get('http://localhost:9000/users/patientDetails')
     .then((res) => { const storage = res.data;
              this.setState({storage})
-    })
-    axios
-    .get('http://localhost:9000/users/patientHistory')
-    .then((res) => { const historyStorage = res.data;
-             this.setState({historyStorage})
     })
     
 
@@ -198,13 +214,30 @@ class HomePage extends Component {
    
   }
   
+  getHistoryInfo (histName) {
+    
+    this.setState({histName})
+    this.componentHistoryMount(histName)
 
+  }
   
- 
+  componentHistoryMount (histName) {
+    console.log("api history input:",histName.name)
+  axios
+  .get(`http://localhost:9000/users/patientHistory`, {
+    params: {
+      histName: histName.name
+    }
+  })
+  .then((res) => { 
+    const historyStorage = res.data;
+           this.setState({historyStorage})
+  })
+  
+  }
 
 
-
-  onSub = () => {
+onSub = () => {
     
         this.props.history.push('/sign-up');
     
@@ -250,6 +283,7 @@ deleteDisplayHide = () => {
 
 showWeekGraph = () => {
   this.setState({ weekDisplay: true });
+  
 }
 
 hideWeekGraph = () => {
@@ -264,6 +298,12 @@ hideDailyGraph = () => {
   this.setState({ dailyDisplay: false });
 }
 
+//name calling
+sendNameval (name) {
+  const sendName = name
+  console.log('DataL',sendName)
+  
+}
 
   render() {
   const show = this.state.show 
@@ -286,7 +326,7 @@ hideDailyGraph = () => {
   const hideDailyGraph = this.state.hideDailyGraph
   const displayDayData = this.state.displayDayData
   const textFil = this.state.textFil
-
+  
     const { nm } = this.props.location
     const { wht } = this.props.location
     const { hht } = this.props.location
@@ -296,9 +336,9 @@ hideDailyGraph = () => {
     const {historyStorage} = this.state;
     
     const { data: chartData } = this.state;
-    
-    
-
+    const {data: historyTable} = this.state
+    const {dispHistoryName} = this.state
+    const {rows} = this.state
     const { dailyData: chartDailyData } = this.state;
    // let rows = []
     //const data = this.state;
@@ -312,12 +352,15 @@ hideDailyGraph = () => {
        
      }
     */}
+    const {patientDummyData: dummyDetails} = this.state;
+    const histName = this.state;
+    
+    const sendName = this.state.sendName
    
-
     const columns = [{
       dataField: 'name',
       text: 'Name',
-      filter: textFilter() 
+      //filter: textFilter() 
     }, {
       dataField: 'weight',
       text: 'Weight',
@@ -326,24 +369,37 @@ hideDailyGraph = () => {
       dataField: 'height',
       text: 'Height',
       sort: true
+    },
+    {
+      dataField: 'limit',
+      text: 'Limit',
+      sort: true
+    },
+    {
+      dataField: 'description',
+      text: 'Description',
+      sort: true
     }];
 
     const selectRow = {
       mode: 'radio',
-      clickToSelect: true
+      clickToSelect: true,
+      onSelect: (rows) => {
+        console.log("values:",rows)
+        this.setState({rows})
+        console.log(rows.name)
+        //this.getHistoryInfo(rows.name)
+        //this.componentHistoryMount(rows)
+      },
+     
     };
-
-    const columns2 = [ {
-      dataField: 'name',
-      text: 'Name',
-      filter: textFilter() 
-      
-    },{
-      dataField: 'Date',
+   
+    const columns2 = [{
+      dataField: 'day',
       text: 'Date',
       
     }, {
-      dataField: 'waterConsumption',
+      dataField: 'value',
       text: 'Consumption',
       
     }];
@@ -355,8 +411,6 @@ hideDailyGraph = () => {
       text: '3', value: 3  
     }, {  
       text: '10', value: 10  
-    }, {  
-      text: 'All', value: this.state.storage.length  
     } ],   
 
     sizePerPage: 3,   
@@ -375,9 +429,7 @@ hideDailyGraph = () => {
       text: '3', value: 3  
     }, {  
       text: '10', value: 10  
-    }, {  
-      text: 'All', value: this.state.historyStorage.length  
-    } ],   
+    }],   
 
     sizePerPage: 3,   
     pageStartIndex: 0,   
@@ -397,9 +449,7 @@ hideDailyGraph = () => {
  
   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
     <ReactBootStrap.Nav className="mr-auto">
-      <ReactBootStrap.Nav.Link href="/#/smart-cup-app/sign-up">Add patients</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="/#/smart-cup-app/patientList">View patients</ReactBootStrap.Nav.Link>
-    
+      
     </ReactBootStrap.Nav>
     <ReactBootStrap.Nav>
      
@@ -416,30 +466,89 @@ hideDailyGraph = () => {
             
              <div className="childContainer1">
 
-              <div>
-                  <h3>Patient Details</h3>
+              <div className='patientDetailHeadingContainer'>
+                  <h5>Patient Details</h5>
               </div>
              
               <div className="tableDisplay">
              {/* <ReactBootStrap.Table keyField='name' data={storage } columns={ columns } pagination={ paginationFactory() } />*/
              } 
             
-            
-            <BootstrapTable   
-                        striped  
-                        hover
-                        keyField='name'   
-                        data={ storage }   
-                        columns={ columns }   
-                        filter={ filterFactory() } 
-                        selectRow={ selectRow }
-                        pagination={ paginationFactory(options) }
-                        />  
+            <div className='patinetTableContainer'>
+            <Table      striped bordered hover size="md"
                         
+                        className='patientTablesize'
+                        keyField='name'   
+                         
+                           
+                        filter={ filterFactory() } 
+                        //selectRow={ selectRow }
+                        pagination={ paginationFactory(options) }
+                        >
+                          <thead>
+                            <td> </td>
+                            <td>Name</td>
+                            <td>Weight</td>
+                            <td>Height</td>
+                            <td>Limit</td>
+                            <td>Description</td>
+                          </thead>
+                          <tbody>
+                          <tr>
+                            <td><input type='radio' name='select' onClick= {this.sendNameval('John')}/></td>
+                            <td>John Doe</td>
+                            <td>144</td>
+                            <td>6</td>
+                            <td>500</td>
+                            <td>Student</td>
+
+                          </tr>
+                          <tr>
+                            <td><input type='radio'name='select'/></td>
+                            <td>R. Ron</td>
+                            <td>164</td>
+                            <td>6.1</td>
+                            <td>500</td>
+                            <td>Faculty</td>
+
+                          </tr>
+                          <tr>
+                            <td><input type='radio' name='select'/></td>
+                            <td>John k</td>
+                            <td>184</td>
+                            <td>5.3</td>
+                            <td>400</td>
+                            <td>Student</td>
+
+                          </tr>
+                          </tbody>
+                          </Table>
+                          <div className='paginationContainer'>
+                          <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+        <span class="sr-only">Previous</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" >1</a></li>
+    <li class="page-item"><a class="page-link" >2</a></li>
+    <li class="page-item"><a class="page-link" >3</a></li>
+    <li class="page-item">
+      <a class="page-link" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+        <span class="sr-only">Next</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+    </div>                    
+            </div>           
           
-          <div style={{marginLeft:590}}>
+          <div className='buttonContainer'>
           <>
-         <Button type="new" onClick={this.showModal}>New</Button>{' '}
+         <Button type="new" className='newButton' size='sm' onClick={this.showModal}>New</Button>{' '}
          <Modal  animation={false}  show={show}
         onHide={this.hideModal}
         backdrop="static"
@@ -449,11 +558,11 @@ hideDailyGraph = () => {
         </Modal.Header>
         <Modal.Body> 
         <form action="HomePage.js" onSubmit={this.handleSubmit} className="FormFields">
-          <div className="FormField">
+          <div className="">
          
-            <label className="FormField__Label_Modal" htmlFor="name">
+            {/*<label className="FormField__Label_Modal" htmlFor="name">
               Name
-            </label>
+            </label>*/}
             <input
               type="text"
               id="inputName"
@@ -463,13 +572,15 @@ hideDailyGraph = () => {
               value={this.state.inputName}
               onChange={this.handleChange}
             />
+            
           </div>
 
           {/*convert to weight*/}
           <div className="">
-            <label className="FormField__Label_Modal" htmlFor="weight">
+            {/*<label className="FormField__Label_Modal" htmlFor="weight">
               Weight
-            </label>
+          </label>*/}
+            <br/>
             <input
               type="number"
               id="inputWeight"
@@ -482,7 +593,8 @@ hideDailyGraph = () => {
           </div>
 
           <div className="">
-            <label className="FormField__Label_Modal">Height</label>
+            {/*<label className="FormField__Label_Modal">Height</label>*/}
+            <br/>
             <input
               type="number"
               id="inputHeight"
@@ -492,7 +604,36 @@ hideDailyGraph = () => {
               value={this.state.inputHeight}
               onChange={this.handleChange}
             />
-          
+           <div className="">
+           {/*<label className="FormField__Label_Modal">
+              Limit
+            </label>*/}
+            <br/>
+            <input
+              type="number"
+              id="inputLimit"
+              className="FormField__Input_Modal"
+              placeholder="Enter Limit"
+              name="inputLimit"
+              value={this.state.inputLimit}
+              onChange={this.handleChange}
+            />
+            </div>
+            <div className="">
+          {/*<label className="FormField__Label_Modal">
+              Description
+            </label>*/}
+            <br/>
+            <input
+              type="text"
+              id="inputDescription"
+              className="FormField__Input_Modal"
+              placeholder="Enter Description"
+              name="inputDescription"
+              value={this.state.inputDescription}
+              onChange={this.handleChange}
+            />
+          </div>
           </div>
         </form>
                  </Modal.Body>
@@ -508,7 +649,7 @@ hideDailyGraph = () => {
        
       </Modal>
       
-         <Button type="new" onClick={this.deleteDisplay}>Delete</Button>{' '}
+         <Button type="new" size='sm' className='deleteButton' onClick={this.deleteDisplay}>Delete</Button>{' '}
          <Modal  animation={false}  show={deleteDisp}
         onHide={this.deleteDisplayHide}
         backdrop="static"
@@ -523,24 +664,49 @@ hideDailyGraph = () => {
          </>
          
          </div>
-         <div>
-           <h3>History:</h3>
+         <div className='historyNameContainer'>
+           <div className='patientHistName'>
+           <h5> History: 
+             </h5>
+           
+           </div>
            <div>
-            
-           <BootstrapTable   
+             <div className= 'patientHistContainer'>
+           <Table       striped bordered hover size="md"
                         striped  
                         hover
-                        keyField='name'   
-                        data={ historyStorage }   
-                        columns={ columns2 }   
-                        filter={ filterFactory()}
-                        pagination={ paginationFactory(options2)}  
-                        />  
-          
+                        className='patientHistorytable'
+                        keyField='name'>
+             <thead>
+    <tr>
+      
+      <th>Day</th>
+      <th>Consumption</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+      <td>11/2</td>
+      <td>400</td>
+     
+    </tr>
+    <tr>
+    <td>11/3</td>
+    <td>500</td> 
+    </tr>
+    <tr>
+    <td>11/5</td>
+    <td>600</td> 
+    </tr>
+        </tbody>                  
+            </Table>  
+               </div>
           <div>
           <>
-          <Button variant="primary" className="logButton"  onClick={this.displayModal} size="lg" block>
-          Edit or log new information
+          <Button variant="primary" className="logButton"  onClick={this.displayModal} size="sm" block>
+          Edit information
           </Button>
           <Modal
         show={display}
@@ -553,8 +719,10 @@ hideDailyGraph = () => {
         </Modal.Header>
         <Modal.Body>
           <input placeholder="Weight"/><br/><br/>
+          <input placeholder="Height" size="lg"/><br/><br/>
           <input placeholder="Liquid limit"/><br/><br/>
           <input placeholder="Description" size="lg"/>
+           
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={this.closeModal}>
@@ -578,13 +746,14 @@ hideDailyGraph = () => {
    
       <>
     <ButtonGroup className="mb-2">
-    <Button onClick={showWeekGraph}>Weekly Analysis</Button>
-    <Button onClick={hideWeekGraph}>Daily Analysis</Button>
+    <Button onClick={showWeekGraph} size='sm'>Weekly Analysis</Button>
+    <Button onClick={showDailyGraph} size='sm'>Daily Analysis</Button>
     
     </ButtonGroup>
+    <div className='chartContainer'>
     <Paper className="graph">
         <Chart
-          data={historyStorage}
+          data={chartData}
           className="chartWeek"
           show={weekDisplay}
         >
@@ -592,13 +761,14 @@ hideDailyGraph = () => {
           <ValueAxis max={7} />
 
           <BarSeries
-            valueField="waterConsumption"
-            argumentField="Date"
+            valueField="value"
+            argumentField="day"
           />
           <Title text="Weekly Analysis" />
           <Animation />
         </Chart>
       </Paper>
+      </div>
      </>
    {/*Tabs for daily and weelkly analysis*/}
    
@@ -663,7 +833,7 @@ hideDailyGraph = () => {
            //   Logout
            // </button>{" "} 
            // *
-            <Link to="/" className="FormField__Link" style={{color:"", textDecoration:"none"}}>
+            <Link to="/" className="FormField__Link" ={{costylelor:"", textDecoration:"none"}}>
               Logout
             </Link>
             

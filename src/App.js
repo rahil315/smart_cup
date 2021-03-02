8bf872b5dc19fce98f68c683fd46244343101664
Rahil Rathod <66318929+rahil315@users.noreply.github.com>
2021-03-01 19:03:58 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import { HashRouter as Router, Route, Link, NavLink, Redirect, Switch } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
-import SignInForm from "./pages/SignInForm";
-import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./cup_signin_homePage/SignInForm";
 
-import AccountPage from "./pages/AccountPage";
-import HomePage from "./pages/HomePage";
-import patientList from "./pages/patientList";
-import dailyAnalysis from "./pages/dailyAnalysis";
-import weeklyAnalysis from "./pages/weeklyAnalysis";
-import monthlyAnalysis from "./pages/monthlyAnalysis";
-import connectPatient from "./pages/connectPatient"
+
+import HomePage from "./cup_signin_homePage/HomePage";
+
 import "./App.css";
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
@@ -26,13 +21,6 @@ class App extends Component {
           <div className="App__Form">
             <Route exact path="/" component={SignInForm}></Route>
             <Route path="/HomePage" component={HomePage}></Route>
-            <Route path="/patientList" component={patientList}></Route>
-            <Route path="/AccountPage" component={AccountPage}></Route>
-            <Route path="/dailyAnalysis" component={dailyAnalysis}></Route>
-            <Route path="/weeklyAnalysis" component={weeklyAnalysis}></Route>
-            <Route path="/monthlyAnalysis" component={monthlyAnalysis}></Route>
-            <Route path="/connectPatient" component={connectPatient}></Route>
-            <Route path="/sign-up" component={SignUpForm}></Route>
             
           </div>
         </div>
